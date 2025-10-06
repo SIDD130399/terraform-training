d81d8027aa435739186f71aeb45376e000eaa629
Siddhant Khandelwal <siddhantkhandelwal77@gmail.com>
2025-10-07 00:41:11 +0530

    Outputs exercise

--- main.tf
+++ main.tf
@@ -3,23 +3,11 @@ data "akamai_group" "my_group_id" {
     contract_id = "ctr_1-1NC95D"
 }
 
-output "my_group_id" {
-    value = data.akamai_group.my_group_id
-}
-
 data "akamai_property" "my_property" {
     name    = "sikhande.prlab.lol"
     version = "7"
 }
 
-output "my_property" {
-    value = data.akamai_property.my_property
-}
-
 data "akamai_appsec_configuration" "my_sec_configuration" {
     name = "WAF Security File"
-}
-
-output "my_appsec_config" {
-    value = data.akamai_appsec_configuration.my_sec_configuration
 }
---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "my_group_id" {
+    value = data.akamai_group.my_group_id
+}
+
+
+output "my_property" {
+    value = data.akamai_property.my_property
+}
+
+
+output "my_appsec_config" {
+    value = data.akamai_appsec_configuration.my_sec_configuration
+}
